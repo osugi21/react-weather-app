361883c2096649b7fbfb0380fa65a57cd59a49a8
akomathu <pc-504@st-ove.co.jp>
2024-06-07 09:48:13 +0900

    天気予報いったん完成

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ function App() {
   const [weekWeather, setWeekWeather] = useState([]);
   const [city, setCity] = useState("");
   const [addText, setAddText] = useState("")
+  const [weatherIcon , setWeatherIcon] = useState({})
 
   
   const getWeather = () => {
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4fee4b35d6fdd1377d5dffd2d1f3ebe5&units=metric&lang=ja`)
   .then(response => {
+    setWeatherIcon({icon: response.data.weather[0].icon})
     setCurrentWeather({
       description: response.data.weather[0].description,
       temp: response.data.main.temp,
       time: new Date(response.data.dt * 1000).toLocaleString()
     });
-
-    // console.log(response)
+    
+console.log(setWeatherIcon)
+console.log(response)
   })
+
   .catch(error => {
-    console.error('リクエストエラー:', error);
+    setAddText("検索不可");
+    setCurrentWeather({});
+    setWeatherIcon({icon: "09n"})
+    alert("入力した都市名は誤りです")
   });
   
     axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=4fee4b35d6fdd1377d5dffd2d1f3ebe5&units=metric&lang=ja`)
@@ -31,7 +38,8 @@ function App() {
        console.log()
     })
     .catch(error => {
-      console.error('リクエストエラー:', error);
+      setAddText("検索不可");
+      setWeekWeather([]);
     });
   }
 
@@ -42,39 +50,45 @@ function App() {
   const handleClick = (e) => {
     setAddText(city);
     getWeather();
+    setCity("");
   }
-  
+  console.log()
+
   return (
-    <div>
-      <div>
-        <input type='text' value={city} onChange={inputText} />
+    <div className='App'>
+      <div className='input'>
+        <input type='text' value={city} onChange={inputText} placeholder='都市名を入力'/>
         <button onClick={handleClick}>🔍</button>
       </div>
-      <div>
-        <div>
-          <h2>{city}の天気</h2>
+      <div className='now'>
+        <div className='weather-now'>
+          <h2>{addText ? addText + "の天気": "現在の天気"}</h2>
         </div>
-        <div>
+        <div className='now-time'>
+          <div>
+            {addText ? <img src={`https://openweathermap.org/img/wn/${weatherIcon.icon}@2x.png`} /> : null}
+          </div>
+          <div>
         {currentWeather.time && (
           <div>
             {currentWeather.time} - {currentWeather.description}, 気温: {currentWeather.temp}℃
           </div>
         )
       }
+      </div>
         </div>
       </div>
-      <div>
-        <div>
-          <h3>3時間ごとの{city}の天気</h3>
+      <div className='week'>
+        <div className='week-weather'>
+          <h3>{addText ? "3時間ごとの" + addText + "の天気" : "3時間ごとのその都市の天気"}</h3>
         </div>
-      <ul>
+      <ul className='week-time'>
           {weekWeather.slice(0, 9).map((weather, index) => (
             <li key={index}>
               {new Date(weather.dt * 1000).toLocaleString()} - {weather.weather[0].description}, 気温: {weather.main.temp}°C
             </li>
           ))}
         </ul>
-        {/* 検索された場所の一時間ごとの天気を取得 */}
       </div>
     </div>
   );
